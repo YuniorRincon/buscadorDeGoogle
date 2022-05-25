05b36a9b8aedc2fa956b95647043b9df4577a64e
yunior rincon <yuniormrs08@gmail.com>
2022-05-25 05:15:55 -0400

    trabajo terminado

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 var oprimir = document.querySelector(".BTN");
 var seach = document.getElementById("buscador");
 var space = document.getElementById("espacio");
+var lupa = document.getElementById("lupa");
 
-
-var op = oprimir;
-op = document.addEventListener("click",EscribirLetras);
 var letra;
+var op = oprimir;
 
+oprimir = document.addEventListener("click", EscribirLetras);
+lupa.addEventListener("click", internet);
+ 
 
 function EscribirLetras(ev)
 {
-    var contador = ev.srcElement.accessKey
+    var contador = ev.srcElement.accessKey;
 
-    if( contador)
+    if(contador == contador)
     {
         letra = contador;
     }
+   
     if(space == contador)
     {
         letra = " ";
     }
-    seach.value += letra;
+    
+    seach.defaultValue += letra;
+    //console.log(ev);
+}
+
+
+function internet()
+{ 
+    window.location.assign(`https://www.google.com/search?q=${seach.defaultValue}`);
 }
